0b4f2cb6792d8add175a0bb00a2470d9ba625d41
owsei <owseiwastaken@gmail.com>
2022-01-17 10:38:13 -0300

    started bootstrap.sh (to mk config file)

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+echo "" > Cal-config.xmp
+echo "<meta>" >> Cal-config.xmp
+echo "	[xmpver 0.3]" >> Cal-config.xmp
+echo "</meta>" >> Cal-config.xmp
+echo "" >> Cal-config.xmp
+echo "<lang>" >> Cal-config.xmp
+echo "" >> Cal-config.xmp
+echo "</lang>" >> Cal-config.xmp
+echo "" >> Cal-config.xmp
+echo "<config>" >> Cal-config.xmp
+echo "	[lang 'pt-br']" >> Cal-config.xmp
+echo "</config>" >> Cal-config.xmp
+echo "" >> Cal-config.xmp
+echo "<dates>" >> Cal-config.xmp
+echo "" >> Cal-config.xmp
+echo "</dates>" >> Cal-config.xmp
